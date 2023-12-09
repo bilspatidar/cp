066314c97d7b6eed6550e707262a6bb252061499
bilspatidar <96615671+bilspatidar@users.noreply.github.com>
2023-12-09 23:00:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change to the deployment directory
-cd /home/omsaitiffin/cp_sparkhub.in
+cd /home/omsaitiffin/cp_sparkhub_in
 
 # Pull the latest changes from the master branch
 git pull origin master
