38ce1db8106b2073f8de43c8e7dccdef85bd8eb3
suraj9032 <suryaprakashpatidar0@gmail.com>
2023-12-28 13:06:55 +0530

    Transaction api

---
+++ application/models/Transaction_model.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Transaction_model extends CI_Model {
+
+    protected $table      = 'transaction';
+    protected $primaryKey = 'id';
+
+    /**
+     * __construct function.
+     * 
+     * @access public
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();        
+    }
+
+    public function getTransaction($filterData) {
+        $this->db->select("*");
+        $this->db->from($this->table);
+        if(!empty($id)) {
+            $this->db->where($this->primaryKey, $id);
+        }
+
+        if(isset($filterData['id']) && !empty($filterData['id'])){
+			$this->db->like('id',$filterData['id']);
+		}
+
+        if(isset($filterData['merchant_id']) && !empty($filterData['merchant_id'])){
+			$this->db->like('merchant_id',$filterData['merchant_id']);
+		}
+
+        if(isset($filterData['transaction_datetime']) && !empty($filterData['transaction_datetime'])){
+			$this->db->like('transaction_datetime',$filterData['transaction_datetime']);
+		}
+
+        if(isset($filterData['payment_id']) && !empty($filterData['payment_id'])){
+			$this->db->like('payment_id',$filterData['payment_id']);
+		}
+
+        
+        if(isset($filterData['status']) && !empty($filterData['status'])){
+			$this->db->like('status',$filterData['status']);
+		}
+
+        return $this->db->get()->result();
+    }
+
+   
+}
